91f12587c184209df554880f6b5e46e25e5bc9d4
carlokuyper <180145@virtualwindow.co.za>
2022-05-19 08:18:00 +0200

    Initial commit

---
+++ scripts/readAll.php
@@ -0,0 +1,33 @@
+<?php 
+
+include 'scripts/db_connection.php';
+
+$sql = "SELECT * FROM users;";
+$result = mysqli_query($conn, $sql);
+
+$resultCheck = mysqli_num_rows($result);
+
+if($resultCheck > 0){
+
+    while($row = mysqli_fetch_assoc($result)){
+        
+        echo "
+
+        <div class='row_item'>
+            <p><strong>Name: </strong>" . $row['name'] . "</p>
+
+            <p><strong>Age: </strong>" . $row['age'] . "</p>
+            
+            <p><strong>Occupation: </strong>" . $row['occupation'] . "</p>
+
+        <div>
+
+        ";
+
+    }
+
+}else{
+    echo "<h1> No data found in DB</h1>";
+}
+
+?>
